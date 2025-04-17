7c20d3308d269a1d599317a25a9a47365778297f
ChaiyoKung <kananek45@gmail.com>
2025-04-18 00:16:48 +0700

    chore: update prettier configuration for consistent code formatting

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,14 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
+  arrowParens: "always",
+  bracketSameLine: false,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
   plugins: ["prettier-plugin-tailwindcss"],
 };
